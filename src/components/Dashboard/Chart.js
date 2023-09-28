142e7975e3780780607ac44d9bbb7b15c9a4d9ff
Vikas Gautam <priydarshivikas123@gmail.com>
2023-09-28 15:37:59 +0530

    scrollbar working on all pages and also fix chart

--- src/components/Dashboard/Chart.js
+++ src/components/Dashboard/Chart.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { LineChart } from '@mui/x-charts/LineChart';
 
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const uData = [9000, 3000, 2000, 2780, 1890, 2390, 3490];
 const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
 const xLabels = [
   'Page A',
@@ -16,8 +16,8 @@ const xLabels = [
 export default function SimpleLineChart() {
   return (
     <LineChart
-      width={500}
-      height={300}
+      width={950}
+      height={350}
       series={[
         { data: pData, label: 'pv' },
         { data: uData, label: 'uv' },
